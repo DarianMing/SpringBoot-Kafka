bb9c88328c6e1e4eaf128cf3a6390bbeb59820c1
DarainMing <743310059@qq.com>
2019-07-19 23:58:53 +0800

    'done'

--- demo-three/src/main/java/com/lm/three/listener/ReportDataListener.java
+++ demo-three/src/main/java/com/lm/three/listener/ReportDataListener.java
@@ -13,7 +13,28 @@ public class ReportDataListener {
     @KafkaListener(id = Contants.REPORT_DATA_CONTAINER ,
             topics = {Contants.TOPIC_PAGE , Contants.TOPIC_CLICK})
     public void listen (ConsumerRecord<? , ?> record) {
-        log.info("监听到消息记录=================");
+        log.info("listen监听到消息记录=================");
+        log.info("topic= {} "  , record.topic());
+        log.info("offset= {}" , record.offset());
+        log.info("partition = " , record.partition());
+        log.info("key= {}" , record.key());
+        log.info("value= {}" , record.value());
+    }
+
+//    @KafkaListener(id = Contants.REPORT_DATA_CONTAINER ,
+//            topics = {Contants.TOPIC_PAGE , Contants.TOPIC_CLICK})
+//    public void listenSameGroup (ConsumerRecord<? , ?> record) {
+//        log.info("listenSameGroup监听到消息记录=================");
+//        log.info("topic= {} "  , record.topic());
+//        log.info("offset= {}" , record.offset());
+//        log.info("partition = " , record.partition());
+//        log.info("key= {}" , record.key());
+//        log.info("value= {}" , record.value());
+//    }
+
+    @KafkaListener(topics = {Contants.TOPIC_PAGE , Contants.TOPIC_CLICK})
+    public void unSameGroup (ConsumerRecord<? , ?> record) {
+        log.info("unSameGroup监听到消息记录=================");
         log.info("topic= {} "  , record.topic());
         log.info("offset= {}" , record.offset());
         log.info("partition = " , record.partition());
